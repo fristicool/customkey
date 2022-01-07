36b85522f16ae34450cae49f6f3abe1d6f693272
fristicool <74139390+fristicool@users.noreply.github.com>
2022-01-07 22:48:57 +0100

    country update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/create-checkout-session', async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         shipping_address_collection: {
-            allowed_countries: ["US", "UK", "BE", "NL", "DE", "FR"],
+            allowed_countries: ["US", "GB", "BE", "NL", "DE", "FR", "CA"],
         },
         shipping_options: [
             {
